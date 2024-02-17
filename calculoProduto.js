5c8991e453aef56d82ecab99b211dad5ef6a5aea
Renan-De-Paula <ads.renandepaula@gmail.com>
2024-02-16 22:09:13 -0300

    subindo projeto com funçoes

--- calculoProduto.js
+++ calculoProduto.js
@@ -1,14 +1,27 @@
-let precoEtiqueta = 100;
-let formaPagamento = 2;
-console
+function desconto(valor, desconto){
+    return (valor - (valor * (desconto / 100)));
+}
+function juros(valor, desconto){
+    return (valor + (valor * (desconto / 100)));
+}
+
+function pagar(formaPagamento, precoEtiqueta){
 if (formaPagamento === 1) {
-   console.log(precoEtiqueta - (precoEtiqueta * 0.1));
-} else if (formaPagamento === 2){
-    console.log(precoEtiqueta - (precoEtiqueta * 0.15));
+ return (desconto(precoEtiqueta, 10));
+} else if (formaPagamento === 2) {
+    return (desconto(precoEtiqueta, 15));
 } else if (formaPagamento === 3) {
-    console.log(precoEtiqueta);
-    } else if (formaPagamento === 4){
-        console.log(precoEtiqueta + (precoEtiqueta * 0.1));
-    } else {
-        console.log("Forma de pagamento inválida/Cancelar pedido");
- }
+    return (precoEtiqueta);
+} else if (formaPagamento === 4) {
+    return (juros(precoEtiqueta, 10));
+} else {
+    return ("Forma de pagamento inválida/Cancelar pedido");
+}
+}
+
+(function main() {
+let formaPagamento = 2;
+let precoEtiqueta = 100;
+let totalPagar = pagar (formaPagamento, precoEtiqueta);
+console.log("Valor a ser pago: " + totalPagar);
+})();
